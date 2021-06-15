c5d65d5d92aafe63869f28c8afff0c4a34996092
Fron10d <50701917+Fron10d@users.noreply.github.com>
2021-06-15 13:30:18 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import pymysql
 
 mydb = pymysql.connect(
-    host="mysql-32671-0.cloudclusters.net",
-    user="student",
-    passwd="benq1234",
-    port=32671,
-    database="DiplomBase"
+    host=".cloudclusters.net",
+    user="",
+    passwd="",
+    port=,
+    database=""
 )
 mycursor = mydb.cursor()
 
